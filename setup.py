bde0757e14867f0a3ae0ba07c650f29efcdb6f6a
rangehow <a954369920@gmail.com>
2024-11-08 13:54:57 +0800

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="niuload",
-    version="0.2.3",
+    version="0.2.4",
     packages=find_packages(),
     install_requires=[
         "accelerate",
@@ -14,7 +14,7 @@ setup(
     description="A module for balanced loading of large language models",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/rangehow/niuload",
+    url="https://github.com/NiuTrans/NiuLoad",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
